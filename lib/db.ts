6e3b6865176349cc8bbe09dae92556c44042fc80
prem-web.dev <a70064182@gmail.com>
2026-04-20 23:43:28 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -9,7 +9,6 @@ declare global {
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
